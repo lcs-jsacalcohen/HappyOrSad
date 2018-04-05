7be9cce0a38be4052eec68dd53083b2dce2fc925
Santos, Diego <dsantos@lcs-mba3217.lcs.on.ca>
2018-04-05 11:17:59 -0400

    We did all the emoji faces

--- HappyOrSad/main.swift
+++ HappyOrSad/main.swift
@@ -15,3 +15,46 @@ var rawInput = readLine()
 print("You said:")
 print(rawInput)
 
+// Make sure the input is not nil
+// (unwrap the optional)
+guard let input = rawInput else {
+    //if we got here, input is nil, so quit program
+    exit(9) // exit with error code 9
+    
+}
+//print the veriefied input
+print(input)
+
+//loop over each character of the input string
+var happyCount = 0 // track how many happy emoji were found
+var sadCount = 0   // track how many sad emoji were found
+for individualCharacter in input {
+
+   
+    //DEBUG: What character are we looking at now?
+    print(individualCharacter)
+    
+    //is the character Happy, sad, or neither
+    if individualCharacter == "😀" {
+        happyCount += 1
+    } else if individualCharacter == "😁" {
+            happyCount += 1
+    } else if individualCharacter == "😊" {
+        happyCount += 1
+    } else if individualCharacter == "🙂" {
+        happyCount += 1
+        
+    } else if individualCharacter == "😕" {
+        sadCount += 1
+    } else if individualCharacter == "😕" {
+        sadCount += 1
+    } else if individualCharacter == "☹️" {
+        sadCount += 1    }
+    } else if individualCharacter == "😕" {
+        sadCount += 1
+    } else if individualCharacter == "😞" {
+        sadCount += 1
+
+}
+
+
